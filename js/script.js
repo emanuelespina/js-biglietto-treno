02b5c2a757e41f3b55649c8e1c3f82bf8554a72e
emanuelespina <spina.emanuele98@gmail.com>
2025-02-16 12:12:10 +0100

    fead: finish ex

--- js/script.js
+++ js/script.js
@@ -7,16 +7,53 @@
 4. assicurarsi che siano stati inserito solo valori numerici
 */
 
+const numKmStr = prompt("Chilometri che si desidera percorrere");
 
-/* PROCESSO LOGICO:
+if (isNaN(numKmStr) === true) {
+    alert("Devi inserire un valore numerico per il campo Km ricarica la pagina");
+} else {
 
-1. calcolare il prezzo del bliglietto in base al num. di km
-2. se sono minorenni applicare uno sconto del 20%
-3. se sono over 65 applicare uno sconto del 40%
-*/
+    const numKm = parseInt(numKmStr);
 
 
-/* STAMPA RISULTATO:
+    const ageStr = prompt("Età del passeggero");
 
-1. stampare il risultato con dude cifre decimali 
-*/
+    if (isNaN(ageStr) === true) {
+        alert("Devi inserire un valore numerico per il campo età ricarica la pagina");
+    } else {
+
+        const age = parseInt(ageStr);
+
+        console.log(numKm, age);
+
+
+        /* PROCESSO LOGICO:
+        
+        1. calcolare il prezzo del bliglietto in base al num. di km (0.21 € al km)
+        2. se sono minorenni applicare uno sconto del 20%
+        3. se sono over 65 applicare uno sconto del 40%
+        */
+
+        const ticketPrice = numKm * 0.21;
+
+        console.log(ticketPrice);
+
+        let finalPrice;
+
+        if (age < 18) {
+            finalPrice = ticketPrice - (ticketPrice * 20) / 100;
+        } else if (age >= 65) {
+            finalPrice = ticketPrice - (ticketPrice * 40) / 100;
+        } else {
+            finalPrice = ticketPrice;
+        }
+
+
+        /* STAMPA RISULTATO:
+        
+        1. stampare il risultato con dude cifre decimali 
+        */
+
+        console.log(`Il prezzo del bliglietto è: ${finalPrice.toFixed(2)}`);
+    }
+}
