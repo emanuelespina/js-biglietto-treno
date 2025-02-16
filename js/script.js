cb08b1f9ab0953c0f333474ceb2c6b1d1414d462
emanuelespina <spina.emanuele98@gmail.com>
2025-02-16 10:56:53 +0100

    fead: first istruction

---
+++ js/script.js
@@ -0,0 +1,22 @@
+
+/* RACCOLTA DATI:
+
+1. chiedere il num. di km e salvarlo in una costante
+2. chiedere l'età del passeggero e salvarla in una costante
+3. convertire le stringhe in num.
+4. assicurarsi che siano stati inserito solo valori numerici
+*/
+
+
+/* PROCESSO LOGICO:
+
+1. calcolare il prezzo del bliglietto in base al num. di km
+2. se sono minorenni applicare uno sconto del 20%
+3. se sono over 65 applicare uno sconto del 40%
+*/
+
+
+/* STAMPA RISULTATO:
+
+1. stampare il risultato con dude cifre decimali 
+*/
